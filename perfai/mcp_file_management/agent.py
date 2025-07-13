2060c2e1928d78db3bc84e526610799a13fe3143
ak@developer <balaajay19@gmail.com>
2025-07-13 21:58:52 +0530

    Update agent.py

--- perfai/mcp_file_management/agent.py
+++ perfai/mcp_file_management/agent.py
@@ -6,7 +6,7 @@ TARGET_FOLDER_PATH = "/home/butcher/"
 
 root_agent = LlmAgent(
     model='gemini-2.5-flash',
-    name='hello_world',
+    name='mcp_file_management',
     instruction='You are a helpful file management assistant. You can read_file,read_multiple_files,write_file,edit_file,create_directory,list_directory,move_file,search_files,get_file_info,list_allowed_directories. Be polite and informative. Always start by greeting the user with all your abilities with function parameters',
     tools=[
         MCPToolset(
