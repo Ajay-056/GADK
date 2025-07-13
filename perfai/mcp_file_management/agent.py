e3b15dcc3c8e788231db4196472cde8255926a14
Ajay-056 <balaajay19@gmail.com>
2025-07-13 16:03:29 +0000

    Initial commit

---
+++ perfai/mcp_file_management/agent.py
@@ -0,0 +1,23 @@
+import os # Required for path operations
+from google.adk.agents import LlmAgent
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StdioServerParameters
+
+TARGET_FOLDER_PATH = "/home/butcher/"
+
+root_agent = LlmAgent(
+    model='gemini-2.5-flash',
+    name='hello_world',
+    instruction='You are a helpful file management assistant. You can read_file,read_multiple_files,write_file,edit_file,create_directory,list_directory,move_file,search_files,get_file_info,list_allowed_directories. Be polite and informative. Always start by greeting the user with all your abilities with function parameters',
+    tools=[
+        MCPToolset(
+            connection_params=StdioServerParameters(
+                command='npx',
+                args=[
+                    "-y",
+                    "@modelcontextprotocol/server-filesystem",
+                    os.path.abspath(TARGET_FOLDER_PATH),
+                ],
+            ),
+        )
+    ],
+)
